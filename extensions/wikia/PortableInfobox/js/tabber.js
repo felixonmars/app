74a7cd95581afca2badb8060b0596eb8dffd2396
Diana <dianafa@o2.pl>
2015-09-18 22:40:29 +0000

    hacked slider

--- extensions/wikia/PortableInfobox/js/tabber.js
+++ extensions/wikia/PortableInfobox/js/tabber.js
@@ -1,30 +1,90 @@
 require([
-	'wikia.window', 'wikia.document', 'wikia.tracker', 'jquery'
-], function(win, doc, tracker, $) {
+	'wikia.window', 'wikia.document', 'wikia.tracker'
+], function(win, doc, tracker) {
+	var slider,
+		imageNumber,
+		imageWidth = 270,
+		currentPostion = 0,
+		currentImage = 0;
 
-	var current = 0,
-		cleanClassName = 'infobox-tabber-item',
-		active = ' active',
-		images = $('.' + cleanClassName),
-		lastElem = images.length-1;
+	function init() {
+		slider = doc.getElementsByClassName('image_slider')[0];
 
-	if ( images[current] ) {
-		images[current].className += active;
+		if (!slider) {
+			return;
+		}
+
+		imageNumber = slider.children.length;
+
+		slider.style.width = parseInt(imageWidth * imageNumber) + 'px';
+
+		doc.getElementsByClassName('prev')[0].onclick = function(){ onClickPrev(); };
+		doc.getElementsByClassName('next')[0].onclick = function(){ onClickNext(); };
+
+		generatePager(imageNumber);
 	}
 
-	$('.prev, .next').click(handleArrowClick);
-
-	function handleArrowClick() {
-		images[current].className = cleanClassName;
-		this.className === 'prev' ? handleNextArrow() : handlePrevArrow();
-		images[current].className += active;
+	function animate(opts){
+		var start = new Date;
+		var id = setInterval(function(){
+			var timePassed = new Date - start,
+				progress = timePassed / opts.duration;
+			if (progress > 1) {
+				progress = 1;
+			}
+			var delta = opts.delta(progress);
+			opts.step(delta);
+			if (progress == 1) {
+				clearInterval(id);
+				opts.callback();
+			}
+		}, opts.delay || 100);
 	}
 
-	function handleNextArrow() {
-		current === lastElem ? current = 0 : current++;
+	function slideTo(imageToGo) {
+		var numOfImageToGo = imageToGo - currentImage;
+		currentPostion = -1 * currentImage * imageWidth;
+
+		var opts = {
+			duration: 500,
+			delta: function(p){return p;},
+			step: function(delta) {
+				slider.style.left = parseInt(currentPostion - delta * imageWidth * numOfImageToGo) + 'px';
+			},
+			callback:function(){
+				currentImage = imageToGo;
+			}
+		};
+
+		animate(opts);
 	}
 
-	function handlePrevArrow() {
-		current === 0 ? current = lastElem : current--;
+	function onClickPrev() {
+		var targetIndex = currentImage == 0 ? imageNumber - 1 : currentImage - 1;
+
+		slideTo(targetIndex);
 	}
+
+	function onClickNext() {
+		var targetIndex = currentImage == imageNumber - 1 ? 0 : currentImage + 1;
+
+		slideTo(targetIndex);
+	}
+
+	function generatePager(imageNumber) {
+		var i, li, pagerDiv = doc.getElementsByClassName('pi-pager')[0];
+
+		for (i = 0; i < imageNumber; i++){
+			li = doc.createElement('li');
+			pagerDiv.appendChild(li);
+
+			li.onclick = function(i) {
+				return function() {
+					slideTo(i);
+				}
+			} (i);
+		}
+	}
+
+	window.onload = init;
 });
